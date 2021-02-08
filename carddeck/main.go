69a08d506f7a765dbb68cbfca95b114bc1864dfc
Keith <k.shintaku@gmail.com>
2021-02-07 20:16:14 -0800

    more with slices and functions

--- carddeck/main.go
+++ carddeck/main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
+	hand, remainingCards := deal(cards, 5)
 
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
 }
